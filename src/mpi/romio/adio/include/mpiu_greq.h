e077d41d3839f2b34de7496fce0894145719f412
Wei-keng Liao <wkliao@users.noreply.github.com>
2019-07-26 00:35:35 -0500

    Check the followings when ROMIO is built stand alone.
    Whether the MPI C compiler is MPICH or Open MPI based
    
    Check following types and defines from mpi.h, which may
    be different between MPICH and OpenMPI.
    MPI_FILE_DEFINED, MPI_OFFSET, MPI_Datarep_conversion_function,
    MPI_Datarep_extent_function, MPI_FILE_NULL, MPIX_Grequest_class
    
    Adjust MPL include and lib paths
    Add MPL library to libromio.a

--- src/mpi/romio/adio/include/mpiu_greq.h
+++ src/mpi/romio/adio/include/mpiu_greq.h
@@ -8,6 +8,10 @@
 #ifndef MPIU_GREQ_H_INCLUDED
 #define MPIU_GREQ_H_INCLUDED
 
+#if !defined(ROMIO_INSIDE_MPICH) && !defined(HAVE_MPIX_GREQUEST_CLASS)
+typedef int MPIX_Grequest_class;
+#endif
+
 int MPIU_Greq_query_fn(void *extra_state, MPI_Status * status);
 int MPIU_Greq_free_fn(void *extra_state);
 int MPIU_Greq_cancel_fn(void *extra_state, int complete);
