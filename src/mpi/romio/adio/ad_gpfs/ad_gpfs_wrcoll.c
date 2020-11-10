d5e40a91352a38afad6485b583a0a32f7f3e3087
qiao.kang@eecs.northwestern.edu <qkt561@guru8.ece.northwestern.edu>
2020-11-09 22:04:34 -0600

    test

--- src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
+++ src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
@@ -1383,9 +1383,9 @@ static void ADIOI_TAM_W_Exchange_data_alltoallv(ADIO_File fd, const void *buf, c
     gpfsmpio_prof_cw[GPFSMPIO_CIO_T_DEXCH_SETUP] += MPI_Wtime() - io_time;
 
     io_time = MPI_Wtime();
-
+/*
     ADIOI_TAM_Kernel(fd, myrank, tmp_buf, send_buf, send_buf_start, send_size, recv_size, nprocs_recv, send_total_size, sum_recv, coll_bufsize, partial_recv, others_req, count, start_pos);
-
+*/
 
     gpfsmpio_prof_cw[GPFSMPIO_CIO_T_DEXCH_NET] += MPI_Wtime() - io_time;
     io_time = MPI_Wtime();
