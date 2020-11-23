95120913c4ac064413a81b9e2a8e0de15cd79887
qiao.kang@eecs.northwestern.edu <qkt561@guru8.ece.northwestern.edu>
2020-11-22 19:08:10 -0600

    teest

--- src/mpi/romio/adio/common/ad_read_coll.c
+++ src/mpi/romio/adio/common/ad_read_coll.c
@@ -1143,7 +1143,7 @@ static void ADIOI_TAM_R_Exchange_data(ADIO_File fd, void *buf, char* agg_buf, in
         }
     }
     ADIOI_TAM_Read_Kernel(fd, myrank, agg_buf, recv_buf, recv_buf_start, send_size, recv_size, nprocs_send, recv_total_size, sum_send, coll_bufsize, partial_send, others_req, count, start_pos);
-/*
+
     if (nprocs_recv) {
         if (buftype_is_contig) {
             for (i = 0; i < nprocs; i++) {
@@ -1159,9 +1159,9 @@ static void ADIOI_TAM_R_Exchange_data(ADIO_File fd, void *buf, char* agg_buf, in
                                    min_st_offset, fd_size, fd_start, fd_end, buftype_extent);
         }
     }
-*/
 
 
+    #if 0
     /* End of intra-node aggregation phase */
     int j, k, tmp = 0;
     char** recv_buf2;
@@ -1292,7 +1292,7 @@ static void ADIOI_TAM_R_Exchange_data(ADIO_File fd, void *buf, char* agg_buf, in
         }
         ADIOI_Free(recv_buf2);
     }
-
+    #endif
 
     if (nprocs_recv) {
         ADIOI_Free(recv_buf_start);
