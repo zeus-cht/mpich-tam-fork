5f1b7bf7a9e43a3c21bbbc09c5906f5b34668882
Qiao Kang <qiaokang2015@u.northwestern.edu>
2020-12-08 20:00:34 -0600

    Update ad_bv_io_list.c

--- src/mpi/romio/adio/ad_benvolio/ad_bv_io_list.c
+++ src/mpi/romio/adio/ad_benvolio/ad_bv_io_list.c
@@ -776,18 +776,20 @@ int ADIOI_BV_StridedListIO(ADIO_File fd, void *buf, int count,
         /* Run list I/O operation */
         if (rw_type == READ_OP) {
 
-            ADIOI_BV_TAM_pre_read(fd, buf_ol_count, buf_len_arr, file_ol_count, file_off_arr, file_len_arr, &local_file_offset, &local_offset_length, &number_of_requests, &local_data_size);
-/*
+            //ADIOI_BV_TAM_pre_read(fd, buf_ol_count, buf_len_arr, file_ol_count, file_off_arr, file_len_arr, &local_file_offset, &local_offset_length, &number_of_requests, &local_data_size);
+
             response =
                 bv_read(fd->fs_ptr, fd->filename, buf_ol_count, (const char **) buf_off_arr,
                             buf_len_arr, file_ol_count, file_off_arr, file_len_arr);
-*/
+
+/*
             if (fd->is_local_aggregator) {
                 response =
                     bv_read(fd->fs_ptr, fd->filename, 1, (const char **) &(fd->local_buf),
                                 &local_data_size, number_of_requests, local_file_offset, local_offset_length);
             }
-            ADIOI_BV_TAM_post_read(fd, buf, count, datatype, buf_ol_count, (const char **) buf_off_arr, buf_len_arr);
+*/
+            //ADIOI_BV_TAM_post_read(fd, buf, count, datatype, buf_ol_count, (const char **) buf_off_arr, buf_len_arr);
         } else {
             /* Local aggregators gather I/O requests from the processes they are responsible for. */
             ADIOI_BV_TAM_write(fd, buf, count, datatype, buf_ol_count, (const char **) buf_off_arr, buf_len_arr, file_ol_count, file_off_arr, file_len_arr, &local_file_offset, &local_offset_length, &number_of_requests, &local_data_size);
