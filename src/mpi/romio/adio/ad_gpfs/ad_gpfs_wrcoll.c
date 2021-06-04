c3f6b4a8883b629c8e97c79e8a3b1040701930df
Qiao Kang <qiaokang2015@u.northwestern.edu>
2021-06-04 16:22:07 -0500

    Update ad_gpfs_wrcoll.c

--- src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
+++ src/mpi/romio/adio/ad_gpfs/ad_gpfs_wrcoll.c
@@ -473,12 +473,12 @@ void ADIOI_GPFS_WriteStridedColl(ADIO_File fd, const void *buf, int count,
     #if TIME_PROFILING==1
     start_time = MPI_Wtime();
     #endif
-/*
+
     ADIOI_Exch_and_write(fd, buf, datatype, nprocs, myrank,
                          others_req, offset_list,
                          len_list, contig_access_count, min_st_offset,
                          fd_size, fd_start, fd_end, buf_idx, error_code);
-*/
+
     #if TIME_PROFILING==1
     fd->exchange_write += MPI_Wtime() - start_time;
     #endif
